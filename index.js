bc86e9b6eb72449bfa7ee8b58a4cae2108cb3c50
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2021-10-30 21:57:28 +0600

    order api and delete api addedd

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ async function run() {
     console.log("Hitting Database");
     const database = client.db("tourGalaxyDB");
     const tourCollection = database.collection("tours");
+    const orderCollection = database.collection("orders");
 
     // GET ALL TOURS API
     app.get("/tours", async (req, res) => {
@@ -28,12 +29,33 @@ async function run() {
       const tours = await cursor.toArray();
       res.send(tours);
     });
+    // GET ALL ORDERS API
+    app.get("/orders", async (req, res) => {
+      const cursor = orderCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
     //GET SINGLE DATA API
     app.get("/tours/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const tour = await tourCollection.findOne(query);
       res.send(tour);
+    });
+
+    //ADD ORDERS API
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.json(result);
+    });
+
+    //DELETE API
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
     });
   } finally {
     // await client.close();
